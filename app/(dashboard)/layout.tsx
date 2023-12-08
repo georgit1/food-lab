c261d4c1c5a5473649bbae62e1de2d61181c9b6d
Georg Reinthaler <georgreinthaler60.gr@gmail.com>
2023-12-08 09:14:48 +0100

    add max-w to main layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,16 +1,18 @@
-import { Navbar } from './_components/Navbar';
-import { Sidebar } from './_components/Sidebar';
+import { Navbar } from "./_components/Navbar";
+import { Sidebar } from "./_components/Sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-full z-50'>
-      <div className='h-[80px] md:pl-56 fixed inset-y-0 w-full z-50'>
+    <div className="z-50 h-full">
+      <div className="fixed inset-y-0 z-50 h-[80px] w-full md:pl-56">
         <Navbar />
       </div>
-      <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
+      <div className="fixed inset-y-0 z-50 hidden h-full w-56 flex-col md:flex">
         <Sidebar />
       </div>
-      <main className=' p-6 md:pl-[248px] pt-[104px] h-full'>{children}</main>
+      <main className="mx-auto h-full max-w-7xl p-6 pt-[104px] md:pl-[248px]">
+        {children}
+      </main>
     </div>
   );
 };
